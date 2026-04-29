76119e5da2c609188abd4f2e197d37ee4949a646
den1zer <denizershar@gmail.com>
2026-04-29 17:32:28 +0300

    fix: URL fixing for deploy

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,8 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+// Expose bridge APIs to renderer, including server URL for dynamic links
 contextBridge.exposeInMainWorld('electron', {
   apiRequest: (opts) => ipcRenderer.invoke('api-request', opts),
   openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
   onServerReady: (cb) => ipcRenderer.on('server-ready', cb),
-  onServerError: (cb) => ipcRenderer.on('server-error', (_e, msg) => cb(msg))
+  onServerError: (cb) => ipcRenderer.on('server-error', (_e, msg) => cb(msg)),
+  serverUrl: process.env.SERVER_URL || `http://127.0.0.1:${process.env.SERVER_PORT || 5000}`
 });
